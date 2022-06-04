3732fd9786fe873911a1b6865d1da846b15cab2a
Trevor Sewell <trev@isnotanumber.com>
2022-06-04 09:53:13 +0100

    initial import and package skeleton

---
+++ src/Providers/LensServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Laraflux\Lens\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LensServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
